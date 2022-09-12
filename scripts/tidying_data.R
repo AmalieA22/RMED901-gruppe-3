cc4e01ae548da7145f5f9a6bf4a648811303959d
Amalie Austgulen <aau019@uib.no>
2022-09-12 11:52:06 +0200

    Exploring the data

--- scripts/tidying_data.R
+++ scripts/tidying_data.R
@@ -15,3 +15,21 @@ library(here)
 data_nontidy <- read.delim(here("data", "copy_exam_nontidy.txt"))
 
 data_nontidy
+
+#Exploring the data
+head(data_nontidy)
+tail(data_nontidy)
+
+summary(data_nontidy)
+
+glimpse(data_nontidy)
+
+skimr::skim(data_nontidy)
+
+naniar::gg_miss_var(data_nontidy)
+
+
+
+
+
+#Tidying the data
